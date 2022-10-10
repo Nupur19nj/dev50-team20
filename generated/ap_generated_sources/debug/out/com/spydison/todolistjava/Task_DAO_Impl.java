78514ad11553bb0583ea014ecb5dc00c9a7b8129
Nupur Jain <114866308+Nupur19nj@users.noreply.github.com>
2022-10-10 23:43:08 +0530

    Add files via upload

---
+++ generated/ap_generated_sources/debug/out/com/spydison/todolistjava/Task_DAO_Impl.java
@@ -0,0 +1,175 @@
+package com.spydison.todolistjava;
+
+import android.database.Cursor;
+import androidx.lifecycle.LiveData;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class Task_DAO_Impl implements Task_DAO {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Task> __insertionAdapterOfTask;
+
+  private final EntityDeletionOrUpdateAdapter<Task> __deletionAdapterOfTask;
+
+  private final EntityDeletionOrUpdateAdapter<Task> __updateAdapterOfTask;
+
+  public Task_DAO_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfTask = new EntityInsertionAdapter<Task>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `my_Tasks` (`title`,`discr`,`id`) VALUES (?,?,nullif(?, 0))";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Task value) {
+        if (value.getTitle() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getTitle());
+        }
+        if (value.getDiscr() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getDiscr());
+        }
+        stmt.bindLong(3, value.getId());
+      }
+    };
+    this.__deletionAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `my_Tasks` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Task value) {
+        stmt.bindLong(1, value.getId());
+      }
+    };
+    this.__updateAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `my_Tasks` SET `title` = ?,`discr` = ?,`id` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Task value) {
+        if (value.getTitle() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getTitle());
+        }
+        if (value.getDiscr() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getDiscr());
+        }
+        stmt.bindLong(3, value.getId());
+        stmt.bindLong(4, value.getId());
+      }
+    };
+  }
+
+  @Override
+  public void insert(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfTask.insert(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void delete(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfTask.handle(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final Task task) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfTask.handle(task);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public LiveData<List<Task>> getData() {
+    final String _sql = "SELECT * FROM my_Tasks";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[]{"my_Tasks"}, false, new Callable<List<Task>>() {
+      @Override
+      public List<Task> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfDiscr = CursorUtil.getColumnIndexOrThrow(_cursor, "discr");
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final List<Task> _result = new ArrayList<Task>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final Task _item;
+            final String _tmpTitle;
+            if (_cursor.isNull(_cursorIndexOfTitle)) {
+              _tmpTitle = null;
+            } else {
+              _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            }
+            final String _tmpDiscr;
+            if (_cursor.isNull(_cursorIndexOfDiscr)) {
+              _tmpDiscr = null;
+            } else {
+              _tmpDiscr = _cursor.getString(_cursorIndexOfDiscr);
+            }
+            _item = new Task(_tmpTitle,_tmpDiscr);
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            _item.setId(_tmpId);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
